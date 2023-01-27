e7f10be69d7974f684638fc935111c9c85df5744
cda07saba <jeremy.sabadie@2isa.org>
2023-01-27 10:52:16 +0100

    improve of th AddToCad méthod.

--- TP_Panier/Cart.cs
+++ TP_Panier/Cart.cs
@@ -20,22 +20,25 @@ internal class Cart
     public void AddToCart(Article article)
     {
         if (articles.Contains(article))
-            articles.Add(article);
-
-    }
-    /// <summary>
-    /// Méthode qui parcourt la liste d'article et qui calcule son total.
-    /// </summary>
-    /// <returns></returns>
-    public double TotalCalculate()
-    {
-        double Total = 0;
-        for (int i = 0; i < articles.Count; i++)
         {
-            Total = Total + articles[i].Price;
+            Article article = articles.Find(a => a.Equals(article));
+            article.Quantity += 1;
         }
-        return Total;
     }
+}
+/// <summary>
+/// Méthode qui parcourt la liste d'article et qui calcule son total.
+/// </summary>
+/// <returns></returns>
+public double TotalCalculate()
+{
+    double Total = 0;
+    for (int i = 0; i < articles.Count; i++)
+    {
+        Total = Total + articles[i].Price;
+    }
+    return Total;
+}
 
 
 
