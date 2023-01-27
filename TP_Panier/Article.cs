75f0122a57047a71bc9e34e698fe10db28e58ecd
cda07saba <jeremy.sabadie@2isa.org>
2023-01-27 10:02:39 +0100

    -Adding of the total calculate méthod. -Adding of the total addToCart méthod.

---
+++ TP_Panier/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_Panier
+{
+    internal class Article
+    {
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public Cart cart { get; set; }
+
+    }
+}
---
+++ TP_Panier/Cart.cs
@@ -0,0 +1,42 @@
+﻿namespace TP_Panier;
+
+internal class Cart
+{
+    public Guid Number { get; set; }
+    public DateTime creation { get; set; }
+    public float Total { get; private set; }
+    private List<Article> articles = new List<Article>();
+
+    public Cart(Guid Number, DateOnly creation)
+    {
+        this.creation = DateTime.Now;
+        Number = new Guid();
+        this.articles = articles;
+    }
+    /// <summary>
+    /// Méthode pour ajouter l'article créé.
+    /// </summary>
+    /// <param name="article"></param>
+    public void AddToCart(Article article)
+    {
+        if (articles.Contains(article))
+            articles.Add(article);
+
+    }
+    /// <summary>
+    /// Méthode qui parcourt la liste d'article et qui calcule son total.
+    /// </summary>
+    /// <returns></returns>
+    public double TotalCalculate()
+    {
+        double Total = 0;
+        for (int i = 0; i < articles.Count; i++)
+        {
+            Total = Total + articles[i].Price;
+        }
+        return Total;
+    }
+
+
+
+}
